08b0898f6177b233974726e54b6fa833289585da
heiyanbin <heiyanbin@heiyanbindeMBP.lan>
2023-08-29 13:06:24 +0800

    get server ip

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,4 @@
 module.exports = (req, res) => {
-  res.json({ message: 'Hello from the serverless function!' });
+    ip = require('ip').address();
+    res.json({ message: 'Hello from ip ' + ip });
 };
